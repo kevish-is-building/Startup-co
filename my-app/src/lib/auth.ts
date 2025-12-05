47123dfa4de53150537616957cb764d045ec0b68
Kevish Sewliya <prokevish07@gmail.com>
2025-12-05 23:17:53 +0530

    Refactor authentication middleware and update database provider to PostgreSQL

--- my-app/src/lib/auth.ts
+++ my-app/src/lib/auth.ts
@@ -7,7 +7,7 @@ import { prisma } from "../lib/prisma";
  
 export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
-		provider: "sqlite",
+		provider: "postgresql",
 	}),
 	emailAndPassword: {    
 		enabled: true
